f4238970dafdb3b094cf51ec136e70b6e1658b7b
Favioman <carlos76190404@gmail.com>
2025-11-06 15:14:22 -0600

    generando carpetas

---
+++ src/app/components/roles/rolesinsert/rolesinsert.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rolesinsert',
+  imports: [],
+  templateUrl: './rolesinsert.html',
+  styleUrl: './rolesinsert.css',
+})
+export class Rolesinsert {
+
+}
---
+++ src/app/components/roles/roleslistar/roleslistar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roleslistar',
+  imports: [],
+  templateUrl: './roleslistar.html',
+  styleUrl: './roleslistar.css',
+})
+export class Roleslistar {
+
+}
