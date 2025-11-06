47eaa428c326d2ab570538d57d6b2ddf82e24124
Favioman <carlos76190404@gmail.com>
2025-11-06 16:35:32 -0600

    añadiendo partes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Roles } from './components/roles/roles';
+import { Rolesinsert } from './components/roles/rolesinsert/rolesinsert';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'roles',component:Roles,
+        children:[
+            {path:'nuevo',component:Rolesinsert}
+        ]
+
+    }
+];
