0879e252ad64994ec95916f8e2020fce03426048
Favioman <carlos76190404@gmail.com>
2025-11-12 23:15:40 -0600

    añadiendo las clases

---
+++ src/app/components/alertas/alertaslistar/alertaslistar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alertaslistar',
+  imports: [],
+  templateUrl: './alertaslistar.html',
+  styleUrl: './alertaslistar.css',
+})
+export class Alertaslistar {
+
+}
